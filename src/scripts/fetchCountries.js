9e4552aaf8a69a755efc3515d3105840db861767
Naliv88 <nalivpv@gmail.com>
2022-11-08 20:44:14 +0200

    HW 10.1

--- src/scripts/fetchCountries.js
+++ src/scripts/fetchCountries.js
@@ -1,10 +1,11 @@
- export function fetchCountries(name) {  
+export function fetchCountries(name) {  
+   
     return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
-    ).then(response => {
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,)
+      .then(response => {
         if (!response.ok) {
-            throw new Error(response.status);
-          }
+          throw new Error(response.status);
+        }
           return response.json();
         });
 }
